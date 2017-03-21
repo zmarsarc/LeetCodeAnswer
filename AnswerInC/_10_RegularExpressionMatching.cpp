ae7218e97abe6ae56101f2fc8752e10677d4735f
zy <zeng.yu.1993@gmail.com>
2017-03-21 15:07:42 +0800

    + 第十题模板

---
+++ AnswerInC/_10_RegularExpressionMatching.cpp
@@ -0,0 +1,19 @@
+/*
+    就是实现一个正则表达式的解析器
+    RE -> NFA -> DFA -> 压缩DFA -> 表驱动的解析器
+*/
+
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+    public:
+
+    // string s -> 搜索串
+    // string p -> 模式串
+    bool isMatch(string s, string p) {
+
+    }
+}
